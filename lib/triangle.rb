a17185e42d7e13d476e9f50c3cd02f6ef2326b6e
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-11-10 21:30:36 -0500

    commit tree 9fdb8fb25c36b0a8f507ed6c9f0fd246cde24f37

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
   # write code here
-end
+
+  attr_accessor :s1, :s2, :s3, :kind #writer & reader method. good to have both in case you need to re-write these
+
+  def initialize(s1, s2, s3)
+
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end 
+
+  def kind 
+    #kind should return, as a symbol, the type of triangle 
+    #three equal sides = equilateral, 2 equal sides = isosceles, no equal sides = scalene
+    if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2 #conditions to check if triangle is not triangle
+
+    begin  
+      raise TriangleError
+      puts error.message
+      end 
+
+    elsif s1 == s2 && s1 == s3
+      self.kind = :equilateral
+
+    elsif s1 == s2 || s1 == s3 || s2 == s3 
+
+      self.kind = :isosceles 
+    else 
+      self.kind = :scalene 
+    end 
+  end 
+
+  class TriangleError < StandardError
+    # triangle error code
+    puts "this is not a triangle"
+  end
+  
+end 
+
